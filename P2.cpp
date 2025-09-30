7f48d400d7b5e4bb2cc5fdc96faa94ed6d528392
Varnika-06 <varnikastuff234@gmail.com>
2025-09-30 10:15:34 +0530

    Create P2.cpp

---
+++ P2.cpp
@@ -0,0 +1,36 @@
+//Name: Varnika Maurya
+//PRN: 24070123160
+//Exp No.: 20
+//Program No.: 2
+#include<iostream>
+using namespace std;
+void insertionSort(int arr[], int n) {
+    for (int i=1; i<n; i++) {
+        int key=arr[i];
+        int j=i-1;
+        while (j>=0&&arr[j]>key) {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1] =key;
+    }
+}
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Original array: ";
+    for (int i=0; i<n; i++) {
+        cout<<arr[i] <<" ";
+    }
+    insertionSort(arr, n);
+    cout<<"\nSorted array: ";
+    for (inti=0; i<n; i++) {
+        cout<<arr[i] <<" ";
+    }
+    return0;
+
+  /* Output:
+    Original array: 64 25 12 22 11
+    Sorted array: 11 12 22 25 64
+    */
+}
